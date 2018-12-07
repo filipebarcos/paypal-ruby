611328b2b9c9548f54bff8c1b4d8d83d36500df5
Filipe Costa <filipebarcos@gmail.com>
2018-12-07 11:29:32 -0500

    added execute

--- lib/paypal/payment.rb
+++ lib/paypal/payment.rb
@@ -13,10 +13,10 @@ module Paypal
       )
     end
 
-    def execute(payer_id:, options: {})
+    def execute(payment_id:, payer_id:, options: {})
       request.post(
-        "v1/payments/payment/#{payer_id}/execute",
-        options, # body => maybe do something like setup method does
+        "v1/payments/payment/#{payment_id}/execute",
+        build_execute_request_body(options.merge(payer_id: payer_id))
       )
     end
 
@@ -35,12 +35,29 @@ module Paypal
       )
     end
 
+    def valid_options_for_execute(options)
+      options.slice(
+        :payer_id,
+        :total,
+        :currency
+      )
+    end
+
     def build_setup_request_body(options)
       opts = valid_options_for_setup(options)
       { intent: opts[:intent] }.tap do |hash|
         hash[:transactions] = [transaction_hash(opts)]
         hash[:payer] = { payment_method: 'paypal' }
         hash[:redirect_urls] = redirect_urls(opts)
+      end.compact
+    end
+
+    def build_execute_request_body(options)
+      opts = valid_options_for_execute(options)
+      { payer_id: opts[:payer_id] }.tap do |hash|
+        hash[:transactions] = [
+          { amount: amount_hash(options) }
+        ]
       end.compact
     end
 
